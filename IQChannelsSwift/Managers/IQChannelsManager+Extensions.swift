d6aab3827d45cb0d661c2f1c167fbe84b3921809
Mish0kk <zinkov04@bk.ru>
2026-07-07 22:04:59 +0700

    Updated version

--- IQChannelsSwift/Managers/IQChannelsManager+Extensions.swift
+++ IQChannelsSwift/Managers/IQChannelsManager+Extensions.swift
@@ -535,7 +535,7 @@ extension IQChannelsManager {
                 self.detailViewModel?.enableAnimMessages = true
             }
             
-            let message = IQMessage(text: "2.3.4-rc1", localID: nextLocalId(), clientID: selectedChat.auth.auth.client?.id)
+            let message = IQMessage(text: "2.3.4", localID: nextLocalId(), clientID: selectedChat.auth.auth.client?.id)
             
             messages.append(message)
             DispatchQueue.main.async {
